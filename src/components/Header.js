fc6518b7c159c1461ad7761c1a66f422e600d40f
Code With Suva <Suvamsen2004@gmail.com>
2025-03-21 22:03:15 +0530

    added online offline features

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 import { LOGO_URL } from "../utils/constant";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
+  const onlineStatus = useOnlineStatus();
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -10,12 +13,15 @@ const Header = () => {
       <div className="nav-items">
         <ul>
           <li>
+            Status: {onlineStatus ? "🟢" : "🔴"}
+          </li>
+          <li>
             <Link to="/">Home</Link>
           </li>
           <li>
             <Link to="/about">About</Link>
           </li>
-          <li>
+          <li className="text-green" >
             <Link to="/contact">Contact</Link>
           </li>
           <li>
